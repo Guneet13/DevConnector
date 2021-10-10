4e2d7d4f720176ba203c50336b0c9bcea1b823d8
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-10-10 11:02:33 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');//imported express library
+const app = express();// instance of express
+
+//lets write our first route
+app.get('/', (req,res) => res.send('Hello'));// anonymous function
+
+const port =5000;
+app.listen(port, ()=> console.log(`server is running on port${port}`));// callback

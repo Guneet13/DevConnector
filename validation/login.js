e781a9c18acbde68c47d582379134df9680ae8b7
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-11-12 13:57:29 -0800

    Created profileSchema and update validate user login

--- validation/login.js
+++ validation/login.js
@@ -4,23 +4,23 @@ const isEmpty = require('./is-empty');
 module.exports = function validateLoginInput(data){
 
     let errors = {}; //to collect errors
-
-    //check email
-    if(isEmpty(data.email)){
-        errors.email = 'Email field is required';
-    }
     
     if(!Validator.isEmail(data.email)){
         errors.email = 'Please put in a valid email';
+    }
+    //check email
+    if(isEmpty(data.email)){
+        errors.email = 'Email field is required';
     }
 
     //check password
     if(isEmpty(data.password)){
         errors.password = 'password field is required';
-    }else if(!Validator.isLength(data.password, {min:8, Max:30})){
+    }
+    if(!Validator.isLength(data.password, {min:8, Max:30})){
         errors.password = 'Password must be between 8 and 30 characters';
     }
-    
+
     return {
       errors,
       isValid: isEmpty(errors)//^^true or false;for easier check externally.
